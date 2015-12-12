7c388c3cec1735b77fecefd7b2740bc04ab0fcf0
william-cormac2015 <williamtunguyen2015@gmail.com>
2015-12-12 18:57:49 +0700

    Controller

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -16,100 +16,190 @@
 int amount = 0;
 int LED1 = 13; // Use the onboard Uno LED
 int LED2 = 12;
-int isObstaclePin2 = 7; // This is our input pin
-int isObstaclePin1 = 6; // This is our input pin 1
+int isObstaclePin2 = 1; // This is our input pin
+int isObstaclePin1 = 0; // This is our input pin 1
+int buzzerPin;
 int isObstacle2 = HIGH;  // HIGH MEANS NO OBSTACLE
 int isObstacle1 = HIGH;
+int button1 = 7;
+int button1State = HIGH;
 bool step = false;
-
 bool off;
-void setup() {
+bool alert;
+bool full;
+bool dim;
+int thresh;
+
+void alertPress(){
+  digitalWrite(buzzerPin, LOW);
+  alert = true;
+}
+void fullLightPress(){
+   digitalWrite(LED1, LOW);
+   digitalWrite(LED2, LOW);
+   full = true;
+   dim = false;
+}
+
+void dimLightPress(){
+   digitalWrite(LED1, HIGH);
+   digitalWrite(LED2, LOW);
+   full = false;
+   dim = true;
+  }
+
+void offPress(){
+  digitalWrite(buzzerPin, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  full = false;
+  dim = false;
+  alert =false;
+  }
+//void setup() {
+//  pinMode(LED1, OUTPUT);
+//  pinMode(LED2, OUTPUT);
+//  pinMode(isObstaclePin2, INPUT);
+//  pinMode(isObstaclePin1, INPUT);
+//  Serial.begin(9600);
+//  
+//}
+//
+//void loop() {
+//  isObstacle2 = HIGH;
+//  isObstacle1 = HIGH;
+//  step = false;
+//  isObstacle2 = digitalRead(isObstaclePin2);
+//  isObstacle1 = digitalRead(isObstaclePin1);
+//  
+//  if ((isObstacle2 == LOW))
+//  {
+//    Serial.println("OBSTACLE!! 2, OBSTACLE!! 2");
+//    while(isObstacle2 == LOW){
+//      
+//      isObstacle1 = digitalRead(isObstaclePin1);
+//      isObstacle2 = digitalRead(isObstaclePin2);
+//      
+//      if(isObstacle1 == LOW){
+//        while(true){
+//          isObstacle2 = digitalRead(isObstaclePin2);
+//          if(isObstacle2 == HIGH){
+//            step = true;
+//            break;
+//          }
+//          isObstacle1 = digitalRead(isObstaclePin1);
+//          if(isObstacle1 == HIGH){
+//            step = false;
+//            break;
+//          }
+//        }
+//      }
+//    }
+//    
+//    if(step){
+//      Serial.println("added");
+//      amount++;
+//    }
+//    Serial.println(amount);
+//  } else if ((isObstacle1 == LOW))
+//  {
+//    Serial.println("OBSTACLE!! 1, OBSTACLE!! 1");
+//    while(isObstacle1 == LOW) {
+//
+//      isObstacle1 = digitalRead(isObstaclePin1);
+//      isObstacle2 = digitalRead(isObstaclePin2);
+//
+//      if(isObstacle2 == LOW){
+//        while(true){
+//
+//          isObstacle1 = digitalRead(isObstaclePin1);
+//          if(isObstacle1 == HIGH){
+//            step = true;
+//            break;
+//          }
+//
+//          isObstacle2 = digitalRead(isObstaclePin2);
+//          if(isObstacle2 == HIGH){
+//            step = false;
+//            break;
+//          }
+//        }
+//      }
+//    }
+//    
+//    if(step){
+//        if(amount > 0){
+//          Serial.println("delete");
+//          amount--;
+//        }
+//      }
+//      Serial.println(amount);
+//    }
+//
+//  if(amount == 0){
+//    digitalWrite(LED1, HIGH);
+//    digitalWrite(LED2, HIGH);
+//  } else {
+//    digitalWrite(LED1, LOW);
+//    digitalWrite(LED2, LOW);
+//  }
+//  delay(200);
+//
+//}
+
+void setup()
+{
+  
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
-  pinMode(isObstaclePin2, INPUT);
-  pinMode(isObstaclePin1, INPUT);
   Serial.begin(9600);
-  
+  pinMode(button1,INPUT);
+ 
 }
 
-void loop() {
-  isObstacle2 = HIGH;
-  isObstacle1 = HIGH;
-  step = false;
-  isObstacle2 = digitalRead(isObstaclePin2);
-  isObstacle1 = digitalRead(isObstaclePin1);
+void loop(){
+ button1State = digitalRead(button1);
+ Serial.println(button1State);
+ if (button1State == LOW) {
+  full = true;
   
-  if ((isObstacle2 == LOW))
-  {
-    Serial.println("OBSTACLE!! 2, OBSTACLE!! 2");
-    while(isObstacle2 == LOW){
-      
-      isObstacle1 = digitalRead(isObstaclePin1);
-      isObstacle2 = digitalRead(isObstaclePin2);
-      
-      if(isObstacle1 == LOW){
-        while(true){
-          isObstacle2 = digitalRead(isObstaclePin2);
-          if(isObstacle2 == HIGH){
-            step = true;
-            break;
-          }
-          isObstacle1 = digitalRead(isObstaclePin1);
-          if(isObstacle1 == HIGH){
-            step = false;
-            break;
-          }
-        }
-      }
-    }
-    
-    if(step){
-      Serial.println("added");
-      amount++;
-    }
-    Serial.println(amount);
-  } else if ((isObstacle1 == LOW))
-  {
-    Serial.println("OBSTACLE!! 1, OBSTACLE!! 1");
-    while(isObstacle1 == LOW) {
-
-      isObstacle1 = digitalRead(isObstaclePin1);
-      isObstacle2 = digitalRead(isObstaclePin2);
-
-      if(isObstacle2 == LOW){
-        while(true){
-
-          isObstacle1 = digitalRead(isObstaclePin1);
-          if(isObstacle1 == HIGH){
-            step = true;
-            break;
-          }
-
-          isObstacle2 = digitalRead(isObstaclePin2);
-          if(isObstacle2 == HIGH){
-            step = false;
-            break;
-          }
-        }
-      }
-    }
-    
-    if(step){
-        if(amount > 0){
-          Serial.println("delete");
-          amount--;
-        }
-      }
-      Serial.println(amount);
-    }
-
-  if(amount == 0){
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-  } else {
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, LOW);
   }
-  delay(200);
-
+ 
+  if(!off){  
+    isObstacle1 = analogRead(isObstaclePin1);                        // READ SENSOR A AND B
+    isObstacle2= analogRead(isObstaclePin2);
+    thresh = 500;                                  // !!!!!!!!!!!!!!!   CHANGE THE VALUE OF THRESHOLD ACCORDING TO THE AMBIENT LIGHT
+    if(isObstacle1<thresh && isObstacle2>thresh)
+    {
+      amount++;                                      //  INCREMENT
+      Serial.println("added");
+      Serial.println(amount);
+      delay(900);
+    }
+    
+    if(isObstacle1>thresh && isObstacle2<thresh)
+    {
+      if(amount != 0){
+      amount--;                                       // DECREMENT
+      }
+      Serial.println("deleted");
+      Serial.println(amount);
+      delay(900);
+    }
+  
+    
+    if(amount == 0){
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+    } else {
+      if(full){
+        fullLightPress();
+      } else {
+        dimLightPress();
+      }
+    }
+  } else {
+    offPress();
+  }
 }
+
